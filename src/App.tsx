2924b645afc8533905a2397645a1ec65d2f0f8aa
Alex Burner <aburner@apple.com>
2022-10-12 09:09:38 -0700

    Test out useFrame and ref manipulations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { Canvas } from '@react-three/fiber'
-import { FC } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { FC, useRef } from 'react'
+import { Mesh, Vector3 } from 'three'
 
 export const App: FC = () => (
   <Canvas>
-    <mesh>
+    <SpinningBox />
+  </Canvas>
+)
+
+const ROTATE_AXIS = new Vector3(1, 0, 0)
+
+const SpinningBox: FC = () => {
+  const meshRef = useRef<Mesh>()
+  useFrame((_state, delta) => {
+    meshRef.current?.rotateOnAxis(ROTATE_AXIS, delta)
+  })
+  return (
+    <mesh ref={meshRef}>
       <boxGeometry />
       <meshNormalMaterial />
     </mesh>
-  </Canvas>
-)
+  )
+}
